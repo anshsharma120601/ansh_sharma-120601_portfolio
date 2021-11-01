772e9fc5695a617ad7eb9e8ee4e094ec16d75c3d
Ansh Sharma <anshsharma120601@gmail.com>
2021-11-01 19:28:22 +0530

    finally

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "1d8ef4814942596afbbb";
+/******/ 	var hotCurrentHash = "700f1c9f7065f8768ae7";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- components/Resume.js
+++ components/Resume.js
@@ -4,7 +4,7 @@ function Resume() {
   return (
     <div className={classes.resume}>
       <p>
-        <img src="https://media-exp1.licdn.com/media/AAYUAQR3AAgAAQAAAAAAADrreC7NMJqaTKuHrCSf0-w-XA.png" />
+        <img src="https://instagram.fdel1-2.fna.fbcdn.net/v/t51.2885-15/e15/p480x480/251204509_1100124617399548_3810922499970257398_n.jpg?_nc_ht=instagram.fdel1-2.fna.fbcdn.net&_nc_cat=104&_nc_ohc=HoeLyPVkTQMAX__BpxT&edm=ABJHkxYAAAAA&ccb=7-4&oh=20f64da23b86f5a87f0a088e4788d535&oe=6187A036&_nc_sid=fa978c&ig_cache_key=MjY5NzM3OTU4NDk2NTA4NDI3OA%3D%3D.2-ccb7-4" />
       </p>
     </div>
   );
