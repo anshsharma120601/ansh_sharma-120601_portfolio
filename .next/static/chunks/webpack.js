bd1f3f06995346d1328c0dee3a9f94a282d29c23
Ansh Sharma <anshsharma120601@gmail.com>
2021-11-02 17:08:55 +0530

    OK now OK

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "ace41571a167c6fec333";
+/******/ 	var hotCurrentHash = "0eb7f4d11c19e92caae0";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
