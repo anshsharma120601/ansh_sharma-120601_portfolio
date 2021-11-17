77cc2215b5dcdbbc5a029e3fa4ca9eb78806afb8
Ansh Sharma <anshsharma120601@gmail.com>
2021-11-17 19:35:02 +0530

    OK do It

--- components/Resume.js
+++ components/Resume.js
@@ -10,7 +10,6 @@ function Resume() {
         h={['27rem', '40rem', '40rem', '40rem']}
         w={['18rem', '30rem', '30rem', '30rem']}
         src="https://media-exp1.licdn.com/media/AAYUAQR3AAgAAQAAAAAAAD7uosv67NvLR86tnJPtFrKNVQ.png"
-        alt="Ansh Sharma Resume"
       />
     </Flex>
   );
